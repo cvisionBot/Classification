c07b836b4fff1895b0a9bdaf2a25c7bf6766343e
DRLee <ryol8888@fssolution.co.kr>
2022-01-04 15:57:36 +0900

    fixed resnet.py : remove gather, add convert_onnx.py and onnx_compare.py

---
+++ module/convert_onnx.py
@@ -0,0 +1,29 @@
+import torch 
+import argparse 
+from module.classifier import Classifier 
+from utils.module_select import get_model 
+from utils.yaml_helper import get_train_configs 
+ 
+ 
+def main(cfg): 
+    model = get_model(cfg['model'])(in_channels=3, classes=cfg['classes']) 
+    if torch.cuda.is_available: 
+        model = model.to('cuda') 
+    model_module = Classifier.load_from_checkpoint( 
+        '/ssd2/lyj/resnet50_cls_ckpt/ResNet50_150_Epoch.ckpt', model=model 
+    ) 
+    model_module.eval() 
+ 
+    # Convert PyTorch To ONNX 
+    dumTensor = torch.rand(1, 3, 64, 64) 
+    if  torch.cuda.is_available: 
+        dumTensor = dumTensor.to('cuda') 
+    torch.onnx.export(model_module.model, dumTensor, cfg['model']+'.onnx', 
+                        input_names=['input'], output_names=['pred']) 
+ 
+if __name__ == '__main__': 
+    parser = argparse.ArgumentParser() 
+    parser.add_argument('--cfg', required=True, type=str, help='Train config file') 
+    args = parser.parse_args() 
+    cfg = get_train_configs(args.cfg) 
+    main(cfg)
