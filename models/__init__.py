d6cd3a17e73ac65a8d07f4afab4e49db69ad7983
DRLee <ryol8888@fssolution.co.kr>
2022-01-04 15:31:49 +0900

    merge master to yj-dev

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-from .backbone.resnet import ResNet
-__all__ = ['ResNet']
-=======
 from .backbone.resnet import ResNet
 from .backbone.resnext import ResNeXt
 __all__ = ['ResNet', 'ResNeXt']
->>>>>>> origin/main
